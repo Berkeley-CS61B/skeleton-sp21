8cf2d3e51f090c80bd09fe6840c27f9b142f324e
Itai Smith <iasmith@berkeley.edu>
2021-01-18 16:08:04 -0800

    add snaps files

---
+++ lab1/CheckLabConfig.java
@@ -0,0 +1,99 @@
+import java.io.File;
+
+public class CheckLabConfig {
+	public static void checkConfig(boolean isWindows) {
+		String repoVariableName = "REPO_DIR";
+		String repoDir = System.getenv(repoVariableName);
+		checkVariableNotNull(repoDir, repoVariableName);
+		System.out.println("Validating your " + repoVariableName + 
+			               " environment variable, which is currently set to: " +
+			               repoDir);
+		checkIsValidRepo(repoDir, repoVariableName, isWindows, false);
+
+		String snapsVariableName = "SNAPS_DIR";
+		String snapsRepoDir = System.getenv(snapsVariableName);
+
+		checkVariableNotNull(snapsRepoDir, snapsVariableName);
+		System.out.println("Validating your " + snapsVariableName + 
+			               " environment variable, which is currently set to: " +
+			               snapsRepoDir);
+		checkIsValidRepo(snapsRepoDir, snapsVariableName, isWindows, true);
+	}
+
+	public static void checkVariableNotNull(String value, String name) {
+		if (value == null) {
+			System.out.println("ERROR! Environment variable " + name + " is not set.");
+			System.out.println("If you've already set it using the lab 2 setup directions, " +
+				               "then try restarting your terminal or IntelliJ.");
+			System.exit(1);
+		}
+	}
+
+	public static void checkIsValidRepo(String value, String name,
+										boolean isWindows, boolean snapsCheck) {
+
+		String splitString = "";
+		if (isWindows) {
+			splitString = "\\\\";
+		} else {
+			splitString = "/";
+		}
+
+		String[] tokens = value.split(splitString);
+		String folderName = tokens[tokens.length - 1];
+
+		String pattern;
+		String expected;
+
+		if (!snapsCheck) {
+			pattern = "fa20-s[\\d]+";
+			expected = "fa20-s1234";
+		} else {
+			pattern = "snaps-fa20-s[\\d]+";
+			expected = "snaps-fa20-s1234";
+		}
+
+		if (!folderName.matches(pattern)) {
+			System.out.println("ERROR! Your " + name + " environment variable is incorrect.");
+			System.out.println("The folder name in the end should match this pattern: " + expected);
+			System.exit(1);
+		}
+
+		File file = new File(value);
+
+		boolean isDirectory = file.isDirectory(); 
+		if (!isDirectory) {
+			System.out.println("ERROR! " + value + " is not a valid folder.");
+			System.out.println("Double check that this variable was set correctly.");
+			System.exit(1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Testing configuration. This program only works for the " +
+			               "Fall 2020 edition of this course.");
+
+		String yourOS = System.getProperty("os.name").toLowerCase();
+		String yourOSVersion = System.getProperty("os.version");
+
+		if (yourOS.contains("windows")) {
+			checkConfig(true);
+		} else {
+			checkConfig(false);
+		}
+
+		/*if (yourOS.contains("mac")) {
+			if (yourOSVersion.contains("10.15")) {
+				checkConfig(false);
+			} else {
+				checkConfig(false);
+			}
+		} // for future reference in case we need to test configurations separately
+		     for Mac OS, Catalina, Linux, etc*/
+
+		System.out.println("Your system appears to be configured correctly. You've completed lab 2 setup.");
+	}
+}
+
+
---
+++ lab1/GetEnvironmentVariables.java
@@ -0,0 +1,60 @@
+import java.util.Properties;
+
+public class GetEnvironmentVariables {
+
+    //  Fill in the path to your sp21-s*** folder between the quotes
+    public static String REPO_DIR = "";
+
+    //  Fill in the path to your snaps-sp21-s*** folder between the quotes
+    public static String SNAPS_DIR = "";
+
+    // Fill in the type of your shell by running 'echo $0` in your terminal. It should be zsh or bash.
+    public static String SHELL = "";
+
+    public static void main(String[] args) {
+
+        String catalina = "echo 'export {variable}={value}' >> ~/.zprofile";
+        String mac = "echo 'export {variable}={value}' >> ~/.bash_profile";
+        String linux = "echo 'export {REPO_DIR}={value}' >> ~/.bashrc";
+        String catalinaSource = "source ~/.zprofile";
+        String macSource = "source ~/.bash_profile";
+        String linuxSource = "source ~/.bashrc";
+
+        String yourOS = System.getProperty("os.name").toLowerCase();
+
+        String repo = null;
+        String snaps = null;
+        String source = null;
+        if (yourOS.contains("mac")) {
+            String version = System.getProperty("os.version");
+            if (SHELL.equals("zsh")) {
+                repo = catalina.replace("{variable}", "REPO_DIR").replace("{value}", REPO_DIR);
+                snaps = catalina.replace("{variable}", "SNAPS_DIR").replace("{value}", SNAPS_DIR);
+                source = catalinaSource;
+            } else {
+                repo = mac.replace("{variable}", "REPO_DIR").replace("{value}", REPO_DIR);
+                snaps = mac.replace("{variable}", "SNAPS_DIR").replace("{value}", SNAPS_DIR);
+                source = macSource;
+            }
+        } else if (yourOS.contains("nux")) {
+            repo = linux.replace("{variable}", "REPO_DIR").replace("{value}", REPO_DIR);
+            snaps = linux.replace("{variable}", "SNAPS_DIR").replace("{value}", SNAPS_DIR);
+            source = linuxSource;
+        }
+
+        if (repo == null) {
+            System.out.println();
+            System.out.println("Oops! We couldn't detect your OS. Please reach out to a lab TA or post on Ed so we can help you move forward");
+            return;
+        }
+
+        System.out.println();
+        System.out.println("Keep reading the spec to know what to do with this output");
+        System.out.println("----------------------------------------------------------");
+        System.out.println(repo);
+        System.out.println(snaps);
+        System.out.println(source);
+
+    }
+
+}
