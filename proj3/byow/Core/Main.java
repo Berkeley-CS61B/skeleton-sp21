11c6b58bae40564f210b03c605de9292b54f6dc3
BorenTsai <35507350+BorenTsai@users.noreply.github.com>
2021-04-05 09:27:30 -0700

    Update Main.java

--- proj3/byow/Core/Main.java
+++ proj3/byow/Core/Main.java
@@ -14,7 +14,7 @@ public class Main {
             engine.interactWithInputString(args[1]);
             System.out.println(engine.toString());
         // DO NOT CHANGE THESE LINES YET ;)
-        } else if (args.length == 2 && args[0].equals("-p")) { System.out.println("Coming soon.") } 
+        } else if (args.length == 2 && args[0].equals("-p")) { System.out.println("Coming soon."); } 
         // DO NOT CHANGE THESE LINES YET ;)
         else {
             Engine engine = new Engine();
