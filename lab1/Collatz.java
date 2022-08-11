7912a2db1dfac03aaca7aa2e5c57b6e395c37843
ZestLee <453171530@qq.com>
2022-08-11 16:24:23 +0800

    easy?

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n/2;
         }
     }
 
