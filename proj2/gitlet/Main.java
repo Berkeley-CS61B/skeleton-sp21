4cfebcc4114ca73ba3fe7b995cae8f39a1643b4a
xUser5000 <abdallahar1974@gmail.com>
2024-01-06 19:09:08 +0200

    feat(gitlet): create .gitlet directory structure on `init`

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,5 +1,7 @@
 package gitlet;
 
+import static gitlet.Utils.*;
+
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author xUser5000
  */
@@ -10,27 +12,32 @@ public class Main {
      */
     public static void main(String[] args) {
         if (args.length == 0) {
-            Utils.exitWithMessage("Please enter a command.");
+            exitWithMessage("Please enter a command.");
         }
 
         String command = args[0];
-        switch(command) {
-            case "init":
-                validateNumArgs(args, 1);
-                Repository.init();
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                break;
-            default:
-                System.out.println("No command with that name exists.");
-                System.exit(0);
+
+        try {
+            switch (command) {
+                case "init":
+                    validateNumArgs(args, 1);
+                    Repository.init();
+                    break;
+                case "add":
+                    // TODO: handle gitlet add <file>
+                    break;
+                default:
+                    exitWithMessage("No command with that name exists.");
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(0);
         }
     }
 
     public static void validateNumArgs(String[] args, int n) {
         if (args.length != n) {
-            Utils.exitWithMessage("Incorrect operands.");
+            exitWithMessage("Incorrect operands.");
         }
     }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,9 +1,9 @@
 package gitlet;
 
 import java.io.File;
-import static gitlet.Utils.*;
+import java.io.IOException;
 
-// TODO: any imports you need here
+import static gitlet.Utils.*;
 
 /** Represents a gitlet repository.
  *
@@ -16,10 +16,25 @@ public class Repository {
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
-    public static void init() {
+    public static final File COMMITS_DIR = join(GITLET_DIR, "commits");
+    public static final File BLOBS_DIR = join(GITLET_DIR, "blobs");
+    public static final File BRANCHES_DIR = join(GITLET_DIR, "branches");
+    public static final File HEAD_FILE = join(GITLET_DIR, "HEAD");
+    public static final File STAGED_DIR = join(GITLET_DIR, "staged");
+    public static final File ADDITION_DIR = join(STAGED_DIR, "addition");
+    public static final File REMOVAL_DIR = join(STAGED_DIR, "removal");
+
+    public static void init() throws IOException {
         if (GITLET_DIR.exists()) {
-            Utils.exitWithMessage("A Gitlet version-control system already exists in the current directory.");
+            exitWithMessage("A Gitlet version-control system already exists in the current directory.");
         }
         GITLET_DIR.mkdir();
+        COMMITS_DIR.mkdir();
+        BLOBS_DIR.mkdir();
+        BRANCHES_DIR.mkdir();
+        STAGED_DIR.mkdir();
+        ADDITION_DIR.mkdir();
+        REMOVAL_DIR.mkdir();
+        HEAD_FILE.createNewFile();
     }
 }
