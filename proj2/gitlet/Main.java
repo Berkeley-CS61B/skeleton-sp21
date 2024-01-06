83556b5ed0a442593e7817be550dc0566288eae2
xUser5000 <abdallahar1974@gmail.com>
2024-01-06 03:28:03 +0200

    feat(gitlet): initial skeleton

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -9,16 +9,29 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
-        String firstArg = args[0];
-        switch(firstArg) {
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
+
+        String command = args[0];
+        switch(command) {
             case "init":
                 // TODO: handle the `init` command
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
-            // TODO: FILL THE REST IN
+            default:
+                System.out.println("No command with that name exists.");
+                System.exit(0);
+        }
+    }
+
+    public static void validateNumArgs(String[] args, int n) {
+        if (args.length != n) {
+            System.out.println("Incorrect operands.");
+            System.exit(0);
         }
     }
 }
